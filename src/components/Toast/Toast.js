4bece26464856f18417b3b708c3c1ba25802bd31
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-07-17 02:52:23 +0100

    Added Keyboard and screen reader support for accessibility users

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -29,14 +29,18 @@ function Toast({ id, children, variant }) {
         <Icon size={24} />
       </div>
       <p className={styles.content}>
+        <VisuallyHidden>
+          {variant} - 
+        </VisuallyHidden>
         { children }
       </p>
       <button 
         className={styles.closeButton}
         onClick={() => dismissToast(id)}
+        aria-label="Dismiss Message"
+        aria-live="off"
       >
         <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
     </div>
   );
